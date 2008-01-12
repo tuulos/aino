8a224a030af00f052c3b5e1592304867e478fa0b
Ville Tuulos <tuulos@parvus.pp.htv.fi>
2008-01-11 16:18:07 -0800

    added -eval [handle_q] to the startup command line. This initializes the request handler atom and ensures that list_to_existing_atom will succeed.

--- wp/master/start_erlay.sh
+++ wp/master/start_erlay.sh
@@ -1,4 +1,4 @@
 
 ERLAY=/home/tuulos/src/erlay/
 
-erl -sname erlay +K true -smp on -pa $ERLAY/ebin -pa src -boot $ERLAY/erlay -erlay scgi_port 2222 -erlay erlay_port 3333
+erl -eval "[handle_q]" -sname erlay +K true -smp on -pa $ERLAY/ebin -pa src -boot $ERLAY/erlay -erlay scgi_port 2222 -erlay erlay_port 3333
